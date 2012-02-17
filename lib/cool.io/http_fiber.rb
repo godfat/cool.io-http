d4d1cc7e40076b28da95744585010d259ab4c595
Lin Jen-Shin <godfat@godfat.org>
2012-02-18 00:49:30 +0800

    http_fiber.rb: fix argument

--- lib/cool.io/http_fiber.rb
+++ lib/cool.io/http_fiber.rb
@@ -4,9 +4,9 @@ require 'fiber'
 class Coolio::HttpFiber < Coolio::Http
   def self.request opts={}
     f = Fiber.current
-    super(opts){ |response, headers|
-      yield(response, headers)
-      f.resume
+    super(opts){ |response|
+      yield(response) if block_given?
+      f.resume(response)
     }
     Fiber.yield
   end
