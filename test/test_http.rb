d265966a8e138bba3261234db047ba554543091f
Lin Jen-Shin <godfat@godfat.org>
2012-02-18 04:07:25 +0800

    a more reasonable test

--- test/test_http.rb
+++ test/test_http.rb
@@ -1,33 +1,53 @@
 
+require 'bacon'
 require 'cool.io/http'
+require 'rack'
 
-def request klass, url
-  klass.request(:url => url){ |response|
-    puts "Response: #{response.body}"
-    puts
-    puts " Headers: #{response.headers}"
-    puts "  Status: #{response.status}"
-  }
+Bacon.summary_on_exit
+port = rand(30000) + 1024
+
+pid = Process.fork do
+  $stdout.reopen(IO::NULL)
+  $stderr.reopen(IO::NULL)
+  Rack::Handler::WEBrick.run(
+    lambda{ |env| [200, {'Content-Type' => 'test/ok'}, ['ok']] },
+    :Port => port)
+end
+sleep(0.5)
+at_exit{ Process.kill 2, pid }
+
+module Kernel
+  def eq? rhs
+    self == rhs
+  end
 end
 
-request(Coolio::Http, 'http://google.com')
-Coolio::Loop.default.run
-puts
+describe Coolio::Http do
+  def request klass, url
+    klass.request(:url => url){ |response| assert(response) }
+  end
 
-Fiber.new{
-  request(Coolio::HttpFiber, 'https://google.com')
-  puts "DONE"
-}.resume
-puts "GO"
-Coolio::Loop.default.run
+  def assert response
+    response.body                   .should.eq 'ok'
+    response.headers['CONTENT_TYPE'].should.eq 'test/ok'
+    response.status                 .should.eq 200
+  end
 
-describe 'ssl' do
-  should 'respect ssl?' do
-    port = rand(30000) + 1024
-    serv = TCPServer.new('localhost', port)
+  should 'asynchrony' do
+    request(Coolio::Http, "http://localhost:#{port}")
+    Coolio::Loop.default.run
+  end
+
+  should 'synchrony' do
+    Fiber.new{
+      assert(request(Coolio::HttpFiber, "http://localhost:#{port}"))
+    }.resume
+    Coolio::Loop.default.run
+  end
+
+  should 'ssl' do
     sock = TCPSocket.new('localhost', port)
     Coolio::Http.new(sock)                    .ssl?.should == false
     Coolio::Http.new(sock).extend(Coolio::SSL).ssl?.should == true
-    serv.close
   end
 end
