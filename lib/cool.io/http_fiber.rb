4a0fff19e213dc4eff6c7a4e685b57d4d59be743
Lin Jen-Shin <godfat@godfat.org>
2012-02-22 01:24:54 +0800

    save the client in fiber local storage for other callback to detach it
    Thread.current[:coolio_http_client]

--- lib/cool.io/http_fiber.rb
+++ lib/cool.io/http_fiber.rb
@@ -4,7 +4,10 @@ require 'fiber'
 class Coolio::HttpFiber < Coolio::Http
   def self.request opts={}
     f = Fiber.current
-    super(opts){ |response|
+    # Thread local variables *are* fiber local variables
+    # see: http://bugs.ruby-lang.org/issues/1717
+    #      http://bugs.ruby-lang.org/issues/5750
+    Thread.current[:coolio_http_client] = super(opts){ |response|
       yield(response) if block_given?
       f.resume(response) if f.alive?
     }
